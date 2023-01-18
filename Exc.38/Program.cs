52e259e32a8a6c362f57c70ec6ff750bc9104bff
Andrey Bataev <bataevai@gmail.com>
2023-01-18 20:19:30 +0400

    Exc.29

--- Exc.38/Program.cs
+++ Exc.38/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 38: Задайте массив натуральных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+// Создаем метод формирования случайного массива чисел со значениями: от (left) до (right) 
+
+
+int[] RandArray (int sizeArr, int left, int right) 
+
+   
+        {
+            int[] array = new int[sizeArr];
+
+            for(int i = 0; i < sizeArr; i++)
+                   
+                {                     
+                    array[i] = new Random().Next(left, right + 1);   
+                }
+
+        return array;
+        }
+
+// Создаем метод разности значений между максимальным и мнимальным значением
+
+int DifferenceMaxAndMin (int[] arr) 
+
+           
+        {
+            
+            int diff = 0;
+            int max = 0;
+            
+
+            for (int i = 1; i < arr.Length; i ++)
+            {
+                max = arr[0];
+                    {                     
+                      if (arr[i + 1] > arr[i]) {   //3 2 1 10 0 5 7 11
+                        max = arr[i + 1]; 
+                                      
+        
+                     }
+                      
+                        else {
+                                int temp = arr[i + 1];
+                                arr[i + 1] = arr[i];
+                                arr[i] = temp;
+
+                        }
+
+                    }
+            }
+
+        return max;
+        }
+        
+int[] massive = RandArray(10, 0, 43);
+Console.WriteLine(string.Join(", ", massive));
+
+int mas = DifferenceMaxAndMin(massive);
+
+Console.WriteLine($" Максимальное число {mas}");
+
